918040e20fb99085a44b41e3593ccc199e2e1b29
JEFF <1122571+mfnalex@users.noreply.github.com>
2018-08-13 13:48:41 +0200

    Update JeffFurnaceAlarmPlugin.java

--- src/de/jeffclan/JeffFurnaceAlarm/JeffFurnaceAlarmPlugin.java
+++ src/de/jeffclan/JeffFurnaceAlarm/JeffFurnaceAlarmPlugin.java
@@ -19,12 +19,6 @@ public class JeffFurnaceAlarmPlugin extends JavaPlugin {
 	
 	void createConfig() {
 		this.saveDefaultConfig();
-		
-		getConfig().addDefault("message-when-using-chest","&7Hint: Type &6/chestsort&7 to enable automatic chest sorting.");
-		getConfig().addDefault("message-when-using-chest2","&7Hint: Type &6/chestsort&7 to disable automatic chest sorting.");
-		getConfig().addDefault("message-sorting-disabled","&7Automatic chest sorting has been &cdisabled&7.&r");
-		getConfig().addDefault("message-sorting-enabled","&7Automatic chest sorting has been &aenabled&7.&r");
-		getConfig().addDefault("message-error-players-only","&cError: This command can only be run by players.&r");
 	}
 
 }
